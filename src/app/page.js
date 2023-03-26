37c07485deae11d56c59f436deb4108501112c45
Sebastian Serrano <sebastian@ripio.com>
2023-03-25 23:36:05 -0300

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -1,91 +1,70 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public'
+const { provider, webSocketProvider } = configureChains([polygon, mainnet], [publicProvider()]);
+
+import { LensConfig, production } from '@lens-protocol/react-web';
+import { bindings as wagmiBindings } from '@lens-protocol/wagmi';
+
+const lensConfig = {
+  bindings: wagmiBindings(),
+  environment: production,
+};
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
+import { LensProvider } from '@lens-protocol/react-web';
+
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <WagmiConfig client={client}>
+      <LensProvider config={lensConfig}>
+        <App />
+      </LensProvider>
+      </WagmiConfig>
     </main>
   )
+}
+
+import { useProfile } from '@lens-protocol/react-web';
+import useNestedFollowers from './hooks/useNestedFollowers';
+import Graph from './components/Graph';
+
+function App() {
+  const { data: profile, loading: profileLoading } = useProfile({
+    handle: 'sserrano44.lens',
+  });
+
+  const { data: followers, loading: followersLoading } = useNestedFollowers(
+    'sserrano44.lens',
+    3
+  );
+
+  if (profileLoading || followersLoading) {
+    return <div>Loading...</div>;
+  }
+
+
+  return (
+    <div>
+      <h1>Nested Followers Graph for {profile.name}</h1>
+      <Graph data={followers} />
+    </div>
+  );
 }
